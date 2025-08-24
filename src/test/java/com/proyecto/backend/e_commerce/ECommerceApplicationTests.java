6f4dd36e9f58d7a88016cb29f166251727a807ae
carlos.iglesias <carlos.iglesias@linktic.com>
2025-08-24 00:29:49 -0500

    ci: cambios ajustes

--- src/test/java/com/proyecto/backend/e_commerce/ECommerceApplicationTests.java
+++ src/test/java/com/proyecto/backend/e_commerce/ECommerceApplicationTests.java
@@ -2,8 +2,10 @@ package com.proyecto.backend.e_commerce;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ECommerceApplicationTests {
 
 	@Test
